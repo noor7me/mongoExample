05a6a4d55b6dc1ab3b4474f5982e0a2f26b76f6b
Abdinur Mohamed <noor1me@uw.edu>
2020-02-14 18:36:35 -0800

    Database configure added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
-module.exports = {
-    mongoURL: 'mongodb://localhost:27017/mydb',
-    secret: 'secretKeyGoesHere'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {
+        mongoURL: 'mongodb+srv://noor7me:Qiyaamo_4040@cluster0-7cncw.mongodb.net/test?retryWrites=true&w=majority',
+        secret: 'secretKeyGoesHere'
+    }
+}else {
+    module.exports = {
+        mongoURL: 'mongodb://localhost:27017/mydb',
+        secret: 'secretKeyGoesHere'
+    }
 }
