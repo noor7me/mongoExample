d975d25c0e580cd505f3b2e90bbd910c8a8efd8b
Abdinur Mohamed <noor1me@uw.edu>
2020-02-14 18:04:12 -0800

    intial project files

---
+++ app.js
@@ -0,0 +1,59 @@
+var express       =   require('express');
+const mongoose      =   require('mongoose');
+const bodyParser    =   require('body-parser');
+const path          =   require('path');
+const cors          =   require('cors');
+const passport      =   require('passport');
+
+
+// intialize the here 
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+// define middlewears
+//URL encoded body parse
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+//Json body parser
+app.use(bodyParser.json());
+//cors middlewear
+app.use(cors());
+//set up the static directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// Connect to the database;
+const db = require('./config/keys').mongoURL;
+
+mongoose.connect(db, {useNewUrlParser: true}).then(() => {
+    console.log(`Database Connected Successfully ${db}`);
+}).catch(err => {
+    console.log(`Unable to connect to the database ${db}`);
+});
+
+// app.get('/', function(req, res)  {
+//     res.send("Hello World...2");
+// });
+
+//bring the user registeration router 
+
+const users = require('./routes/api/users');
+app.use('/api/users', users);
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname, 'public/index.html'))
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+    console.log(`Sever started at port ${PORT}`);
+})
+
+
